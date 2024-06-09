52e047dd9d02a0a72bcf4fcc87d221508c197633
pseudoelement <bubamainer@mail.ru>
2024-06-09 23:49:54 +0300

    go install

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pseudoelement/go-sandbox
 
 go 1.22.1
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/pseudoelement/go-sandbox/funcs"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println(funcs.Merge(
-		[]int{1, 2, 3, 0, 0, 0},
-		3,
-		[]int{2, 5, 6},
-		3,
-	))
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":80", r)
 }
