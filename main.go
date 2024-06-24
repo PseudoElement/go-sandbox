eee2384beae7ead9b8e7c215fabe9df868162b73
pseudoelement <bubamainer@mail.ru>
2024-06-24 14:12:49 +0300

    test pprof and trace

--- main.go
+++ main.go
@@ -10,6 +10,72 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// func runProfiling() {
+// 	cpuProfileFile, err := os.Create("./prof/cpu.prof")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer cpuProfileFile.Close()
+
+// 	// Start CPU profiling
+// 	if err := pprof.StartCPUProfile(cpuProfileFile); err != nil {
+// 		panic(err)
+// 	}
+// 	defer pprof.StopCPUProfile()
+
+// 	fmt.Println("CPU profile written to /prof/cpu.prof")
+
+// 	memProfileFile, err := os.Create("./prof/mem.prof")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer memProfileFile.Close()
+
+// 	// Write memory profile to file
+// 	if err := pprof.WriteHeapProfile(memProfileFile); err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Println("Memory profile written to /prof/mem.prof")
+
+// 	// Start tracing
+// 	traceFile, err := os.Create("trace.out")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer traceFile.Close()
+
+// 	if err := trace.Start(traceFile); err != nil {
+// 		panic(err)
+// 	}
+// 	defer trace.Stop()
+
+// 	fmt.Println("Tracing written to /prof/trace.out")
+// }
+
+// func main() {
+// 	runProfiling()
+
+// 	now := time.Now()
+// 	funcs.IsHappy(1999959944342334234)
+// 	fmt.Printf("IsHappy took %v ms!\n", time.Since(now))
+
+// 	heavyCalc()
+
+// 	fmt.Println("Profiling done!")
+// }
+
+// func heavyCalc() {
+// 	for i := 0; i < 1_000; i++ {
+// 		time.Sleep(10 * time.Millisecond)
+// 		randNum := rand.Intn(100)
+// 		square := math.Pow(float64(randNum), 2)
+// 		if i%100 == 0 {
+// 			fmt.Printf("%v ** 2 = %v\n", randNum, square)
+// 		}
+// 	}
+// }
+
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
