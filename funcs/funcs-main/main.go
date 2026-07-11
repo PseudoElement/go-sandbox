2e7f77086ac4d8782914a2b76d164959aee29e79
pseudoelement <bubamainer@mail.ru>
2026-07-11 12:58:34 +0300

    just for test

--- funcs/funcs-main/main.go
+++ funcs/funcs-main/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	t, err := parseTimeString("02.01.06 15:04:05 MST", "11.12.98 22:11:59 MST")
+	t, err := parseTimeString("02.01.06 15:04:05 MST", "10.12.98 22:11:59 MST")
 	// t, err := parseTimeString("2006-01-02 15:04:05 MST", "2008-02-03 17:18:19 MST")
 	// t, err := parseTimeString("Jun 2, 2006 15::04::05 MST", "Dec 23, 2004 21::15::15 MST")
 	log.Println("err: ", err)
