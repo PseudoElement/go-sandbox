39c7535dab3c603eb88f89d902085c4adabb238c
pseudoelement <bubamainer@mail.ru>
2026-07-23 15:08:12 +0200

    add sobes dir

--- main.go
+++ main.go
@@ -1,54 +1,55 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"bytes"
+	"context"
+	"io"
 	"net/http"
-
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"github.com/pseudoelement/go-sandbox/common/constants"
-	"github.com/pseudoelement/go-sandbox/streaming"
+	"sync"
 )
 
 func main() {
-	r := mux.NewRouter()
-	api := r.PathPrefix("/api/v1").Subrouter()
-
-	api.HandleFunc("/greeting", func(w http.ResponseWriter, r *http.Request) {
-		msg := struct {
-			Message string `json:"message"`
-		}{Message: "Server is alive!"}
-
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(msg)
-	}).Methods("GET")
-
-	api.HandleFunc("/tonconnect/manifest/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := mux.Vars(r)["id"]
-		path, ok := constants.URL_TO_MANIFEST_PATH_MAP[id]
-		if !ok {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("Invalid environvent id!"))
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		http.ServeFile(w, r, path)
-	})
-
-	api.HandleFunc("/tonconnect/logo.png", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		http.ServeFile(w, r, "./static/rubic-logo.png")
-	})
-
-	stream := streaming.NewStreamingModule(api)
-	stream.SetRoutes()
-
-	methods := handlers.AllowedMethods([]string{"POST", "GET"})
-	ttl := handlers.MaxAge(3600)
-	origins := handlers.AllowedOrigins([]string{"*"})
-
-	fmt.Println("Listening port :8080")
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(methods, ttl, origins)(api)))
+	println("jell")
 }
+
+
+// func main() {
+// 	r := mux.NewRouter()
+// 	api := r.PathPrefix("/api/v1").Subrouter()
+
+// 	api.HandleFunc("/greeting", func(w http.ResponseWriter, r *http.Request) {
+// 		msg := struct {
+// 			Message string `json:"message"`
+// 		}{Message: "Server is alive!"}
+
+// 		w.WriteHeader(http.StatusOK)
+// 		json.NewEncoder(w).Encode(msg)
+// 	}).Methods("GET")
+
+// 	api.HandleFunc("/tonconnect/manifest/{id}", func(w http.ResponseWriter, r *http.Request) {
+// 		id := mux.Vars(r)["id"]
+// 		path, ok := constants.URL_TO_MANIFEST_PATH_MAP[id]
+// 		if !ok {
+// 			w.WriteHeader(http.StatusOK)
+// 			w.Write([]byte("Invalid environvent id!"))
+// 			return
+// 		}
+// 		w.WriteHeader(http.StatusOK)
+// 		http.ServeFile(w, r, path)
+// 	})
+
+// 	api.HandleFunc("/tonconnect/logo.png", func(w http.ResponseWriter, r *http.Request) {
+// 		w.WriteHeader(http.StatusOK)
+// 		http.ServeFile(w, r, "./static/rubic-logo.png")
+// 	})
+
+// 	stream := streaming.NewStreamingModule(api)
+// 	stream.SetRoutes()
+
+// 	methods := handlers.AllowedMethods([]string{"POST", "GET"})
+// 	ttl := handlers.MaxAge(3600)
+// 	origins := handlers.AllowedOrigins([]string{"*"})
+
+// 	fmt.Println("Listening port :8080")
+// 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(methods, ttl, origins)(api)))
+// }
---
+++ sobes/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"net/http"
+	"sync"
+)
+
+func main() {
+	batchReqs([]string{"localhost:8080", "146.124.34.19:8081"}, []byte(""))
+}
+
+func doRequest(ctx context.Context, addr string, request []byte) ([]byte, error) {
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, "POST", addr, bytes.NewBuffer(request))
+	if err != nil {
+		return []byte(""), err
+	}
+
+	resp, err := client.Do(req)
+	buf, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return buf, nil
+}
+
+type ApiResponse struct {
+	value string
+}
+
+func batchReqs(addrs []string, req []byte) ApiResponse {
+	wg := &sync.WaitGroup{}
+	var resp ApiResponse
+	wg.Add(len(addrs))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for _, addr := range addrs {
+		go func() {
+			defer wg.Done()
+			respBuf, err := doRequest(ctx, addr, req)
+			if err == nil {
+				resp = ApiResponse{value: string(respBuf)}
+				cancel()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	return resp
+}
