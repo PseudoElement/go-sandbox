963764de464065307a79a8beeea8af229dbccfd4
pseudoelement <bubamainer@mail.ru>
2026-08-01 21:40:22 +0200

    add compareSlices

--- sobes/main.go
+++ sobes/main.go
@@ -3,14 +3,56 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"sync"
 )
 
+func compareSlices[T any](s1, s2 []T) bool {
+	buf1, err := json.Marshal(s1)
+	if err != nil {
+		panic(err)
+	}
+	buf2, err := json.Marshal(s2)
+	if err != nil {
+		panic(err)
+	}
+	if len(buf1) != len(buf2) {
+		return false
+	}
+	for i := range len(buf1) {
+		equal := buf1[i] == buf2[i]
+		if !equal {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
+	s1 := []EmptyResp{{Data: []int{223, 22, 35}}}
+	s2 := []EmptyResp{{Data: []int{223, 22, 35}}}
+	log.Println(compareSlices(s1, s2))
+	// checkEmptySlice()
 	// fanOut()
 	// customMutex()
+}
+
+type EmptyResp struct {
+	Data []int `json:"data"`
+}
+
+func checkEmptySlice() {
+	buf := []byte("{\"data\": null}")
+	var emptyResp EmptyResp
+	if err := json.Unmarshal(buf, &emptyResp); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("resp: %+v\n", emptyResp)
 }
 
 func doRequest(ctx context.Context, addr string, request []byte) ([]byte, error) {
