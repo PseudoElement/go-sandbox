530183bd0e981a951041578beb62a021d7e72f5a
pseudoelement <bubamainer@mail.ru>
2026-08-10 16:12:30 +0200

    add event brocker

--- go-patterns/main.go
+++ go-patterns/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
+	main_writeBatches()
 	// runWorkerPool()
 	// runFanIn()
 	// rangeSimpleChan()
---
+++ go-patterns/write-batches.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"time"
+)
+
+func main_writeBatches() {
+	eventDispatcher := NewEventDispatcher(5)
+	// go eventDispatcher.Listen(context.TODO())
+	go eventDispatcher.ListenWithBufferedChan(context.TODO())
+	go func() {
+		time.Sleep(2 * time.Second)
+		eventDispatcher.AddConsumer("127.0.0.3:8081")
+	}()
+
+	// eventDispatcher.AddConsumer("127.0.0.1:8080")
+	// eventDispatcher.AddConsumer("127.0.0.2:8080")
+
+	idx := 0
+	for {
+		idx++
+		time.Sleep(500 * time.Millisecond)
+		str := fmt.Sprintf("Click-%d", idx)
+		event := UserEvent{"user-event", []byte(str)}
+		fmt.Println("Event created", event)
+		eventDispatcher.SendEvent(event)
+	}
+}
+
+func sendDataSomewhere(ctx context.Context, addr string, events []UserEvent) error {
+	for _, event := range events {
+		fmt.Printf("[SLICE] %s sent to %s. Data: %s.\n", event.EventName, addr, string(event.Data))
+	}
+	return nil
+}
+
+func sendDataBuffChanSomewhere(ctx context.Context, addr string, events []UserEvent) error {
+	for _, event := range events {
+		fmt.Printf("[CHANNEL] %s sent to %s. Data: %s.\n", event.EventName, addr, string(event.Data))
+	}
+	return nil
+}
+
+type UserEvent struct {
+	EventName string
+	Data      []byte
+}
+
+type EventsDispatcher struct {
+	writeChan              chan struct{}
+	eventQueueChan         chan UserEvent
+	eventQueue             []UserEvent
+	batchSize              int
+	destinations           []string
+	simpleListenerActive   bool
+	bufferedListenerActive bool
+}
+
+func NewEventDispatcher(batchSize int) *EventsDispatcher {
+	return &EventsDispatcher{
+		writeChan:              make(chan struct{}),
+		eventQueueChan:         make(chan UserEvent, batchSize),
+		eventQueue:             make([]UserEvent, 0),
+		batchSize:              batchSize,
+		destinations:           make([]string, 0),
+		simpleListenerActive:   false,
+		bufferedListenerActive: false,
+	}
+}
+
+func (ed *EventsDispatcher) AddConsumer(addr string) {
+	ed.destinations = append(ed.destinations, addr)
+}
+
+func (ed *EventsDispatcher) SendEvent(event UserEvent) {
+	if ed.bufferedListenerActive {
+		ed.eventQueueChan <- event
+	}
+	if ed.simpleListenerActive {
+		ed.eventQueue = append(ed.eventQueue, event)
+		if len(ed.eventQueue) >= ed.batchSize {
+			ed.writeChan <- struct{}{}
+		}
+	}
+}
+
+func (ed *EventsDispatcher) ListenWithBufferedChan(ctx context.Context) error {
+	ed.bufferedListenerActive = true
+	eventQueue := make([]UserEvent, 0, ed.batchSize)
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case event := <-ed.eventQueueChan:
+			fmt.Println("event__", event)
+			eventQueue = append(eventQueue, event)
+			if len(eventQueue) >= ed.batchSize {
+				for _, addr := range ed.destinations {
+					go sendDataBuffChanSomewhere(context.TODO(), addr, eventQueue[:ed.batchSize])
+				}
+				eventQueue = eventQueue[ed.batchSize:]
+			}
+		}
+	}
+}
+
+func (ed *EventsDispatcher) Listen(ctx context.Context) error {
+	ed.simpleListenerActive = true
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ed.writeChan:
+			lastIdx := math.Min(float64(ed.batchSize), float64(len(ed.eventQueue)))
+			batch := ed.eventQueue[:int(lastIdx)]
+			for _, addr := range ed.destinations {
+				go sendDataSomewhere(context.TODO(), addr, batch)
+			}
+			ed.eventQueue = ed.eventQueue[int(lastIdx):]
+		}
+	}
+}
